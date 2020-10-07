0cc11d646e538fcef7e40a6272b82dcf82cd5bf9
Andy Liu <aliu927@gmail.com>
2020-10-06 22:07:05 -0700

    add field checks

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const is_Container_Empty = [document.getElementById('firstName'),
                             document.getElementById('eMail'), 
                             document.getElementById('passWord')
                         ];
-let email = document.getElementById('eMail');
+let chkEmail = document.getElementById('eMail');
 
 const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                        
@@ -20,9 +20,13 @@ submit.addEventListener('click', () => {
         if(isEmpty.value === ''){
             //console.log('Fields are empty');
             let redContainer = isEmpty.style.borderColor ='red';
-            console.log(redContainer);
-        } else if(isEmpty.value !== '' && !!regex.test(email.value)){
-                    console.log('all systems nominal');
+            //console.log(redContainer);
+        } else if(isEmpty.value !== '' && !!regex.test(chkEmail.value)){
+            let blackContainer = isEmpty.style.borderColor = 'black';
+            alert(`Thank you ${is_Container_Empty[0].value} ${is_Container_Empty[1].value} for your order. Please wait while we log you in.`);
+            break;
+
+            //console.log('All systems nominal');
         } else{
             console.log('check conditions');
         }
